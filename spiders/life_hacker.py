4da06dbfabdb21b9cf156d9bd9f427fa80d39097
Talha <mtalha7274@gmail.com>
2024-01-15 21:31:15 +0500

    exception handling

--- spiders/life_hacker.py
+++ spiders/life_hacker.py
@@ -1,3 +1,4 @@
+import traceback
 from spiders.spider import Spider
 import feedparser
 from dateutil import parser
@@ -6,23 +7,26 @@ from models.news import News
 
 class LifeHackerSpider(Spider):
     def parse(self):
-        feed = feedparser.parse(self.config.feed_urls.life_hacker)
-        for entry in feed.entries:
-            id = entry.id
-            title = entry.title
-            description = entry.description
-            publish_date = parser.parse(entry.published)
-            cover_image = entry.media_thumbnail[0]["url"]
-            link = entry.link
+        try:
+            feed = feedparser.parse(self.config.feed_urls.life_hacker)
+            for entry in feed.entries:
+                id = entry.id
+                title = entry.title
+                description = entry.description
+                publish_date = parser.parse(entry.published)
+                cover_image = entry.media_thumbnail[0]["url"]
+                link = entry.link
 
-            news = News(
-                article_id=id,
-                title=title,
-                description=description,
-                publish_date=publish_date,
-                cover_image=cover_image,
-                link=link,
-            )
-            self.store_in_db(news=news)
-            print(news.id)
-            break
+                news = News(
+                    article_id=id,
+                    title=title,
+                    description=description,
+                    publish_date=publish_date,
+                    cover_image=cover_image,
+                    link=link,
+                )
+                self.store_in_db(news=news)
+                print(news.id)
+                break
+        except Exception as _:
+            traceback.print_exc()
